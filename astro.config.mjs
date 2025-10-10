4eb110ebd62a1eea9e5d3d3dc65bd07f67715b77
Elvis Mao <info@elvismao.com>
2025-10-10 13:33:08 +0800

    refactor: unite layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import icon from "astro-icon";
-
 export default defineConfig({
-  site: "https://sitcon.org",
-  base: "/2026/cfp/",
-  output: "static",
-  trailingSlash: "always",
-  integrations: [icon()],
-  build: {
-    format: "file"
-  }
+	site: "https://sitcon.org",
+	base: "/2026/cfp/",
+	output: "static",
+	trailingSlash: "always",
+	integrations: [],
+	build: {
+		format: "file"
+	}
 });
