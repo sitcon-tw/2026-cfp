4acfa14a5592d74f3cfa2423c3ed165786be7870
Elvis Mao <info@elvismao.com>
2025-10-11 19:30:28 +0800

    feat: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://sitcon.org",
 	base: "/2026/cfp/",
 	output: "static",
 	trailingSlash: "always",
-	integrations: [],
+	integrations: [
+		sitemap({
+			filter: (page) => !page.includes('/private/'),
+			changefreq: 'weekly',
+			priority: 0.8,
+			lastmod: new Date(),
+		})
+	],
 	build: {
 		format: "directory"
 	}
